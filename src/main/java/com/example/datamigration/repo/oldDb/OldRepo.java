0500c456f4ce4ca7f9abc965f440e011ef4a250c
Usevalad <Seva552.seva@gmail.com>
2025-01-15 13:18:20 +0100

    Added repository annotation

--- src/main/java/com/example/datamigration/repo/oldDb/OldRepo.java
+++ src/main/java/com/example/datamigration/repo/oldDb/OldRepo.java
@@ -2,10 +2,12 @@ package com.example.datamigration.repo.oldDb;
 
 import com.example.datamigration.entity.Apartment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface OldRepo extends JpaRepository<Apartment, String> {
 
     List<Apartment> findAllByCreatedAtAfter(LocalDate after);
